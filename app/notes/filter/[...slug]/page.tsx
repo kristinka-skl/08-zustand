18688966ba0d680f6d12f43b82ce809fe5003d59
kristinka-skl <kristinka.skl@gmail.com>
2025-11-01 22:50:55 +0100

    change filter img

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -28,7 +28,7 @@ export async function generateMetadata({
       siteName: "NoteHub",
       images: [
         {
-          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          url: "https://i.ibb.co/hRmh19Gt/Note-Hub-green.png",
           width: 1200,
           height: 630,
           alt: `Notes within the category ${category}`,
